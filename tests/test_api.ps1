9f31021d135a69ff8afce5dfcb837fd4e9e01267
Oleg Mukhin <91499623+BDLabOleg@users.noreply.github.com>
2024-12-15 18:13:23 +0200

    Update test_api.ps1

--- tests/test_api.ps1
+++ tests/test_api.ps1
@@ -4,6 +4,8 @@ $containerId = docker run -d -p 3000:3000 olegmukhin/expense-tracker:latest
 # Затримка для запуску сервісу
 Start-Sleep -Seconds 10  # Збільшуємо час очікування
 
+$response = Invoke-WebRequest -Uri "http://localhost:3000/api/healthcheck" -UseBasicParsing
+
 # Тестуємо API
 docker logs $containerId
 
